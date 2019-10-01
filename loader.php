69513692ed92933d5f7c0fccdca0eb83c97db65b
bomsoul <45492963+bomsoul@users.noreply.github.com>
2019-10-01 16:12:12 +0700

    Add files via upload

---
+++ loader.php
@@ -0,0 +1,17 @@
+<?php
+$con = mysqli_connect("remotemysql.com","ytzEjdpCNf","L9LcYDD81I","ytzEjdpCNf");
+
+if (mysqli_connect_errno($con)) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+  
+ //$userid = $_POST['userid'];
+// $result = mysqli_query($con,"SELECT * From User_Course");
+$result = mysqli_query($con,"SELECT Course.id as id,Course.name as Course,users.name as teacher FROM ((Course INNER JOIN User_Course ON Course.id = User_Course.course_id) INNER JOIN users ON users.id = User_Course.user_id) WHERE users.id = 1");
+
+while($row = mysqli_fetch_array($result)){   
+   echo json_encode($row).",";
+}
+
+ mysqli_close($con);
+?>
