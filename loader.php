0c9ac7e99d69f768579cd3b8781822cef3e0711b
bomsoul <45492963+bomsoul@users.noreply.github.com>
2019-10-05 22:26:40 +0700

    Add files via upload

--- loader.php
+++ loader.php
@@ -5,9 +5,9 @@ if (mysqli_connect_errno($con)) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
  }
   
- //$userid = $_POST['userid'];
+ $userid = $_POST['userid'];
 // $result = mysqli_query($con,"SELECT * From User_Course");
-$result = mysqli_query($con,"SELECT Course.id as id,Course.name as Course,users.name as teacher FROM ((Course INNER JOIN User_Course ON Course.id = User_Course.course_id) INNER JOIN users ON users.id = User_Course.user_id) WHERE users.id = 1");
+$result = mysqli_query($con,"SELECT Course.id as id,Course.name as Course,users.name as teacher FROM ((Course INNER JOIN User_Course ON Course.id = User_Course.course_id) INNER JOIN users ON users.id = User_Course.user_id) WHERE users.id = $userid");
 
 while($row = mysqli_fetch_array($result)){   
    echo json_encode($row).",";
